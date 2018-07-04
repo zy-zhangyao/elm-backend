1b6625ad8a02fcd3675629ed6ee5ea245ac849a0
zhangyao <1593105828@qq.com>
2018-07-04 15:21:33 +0800

    zy1

---
+++ src/components/addproduct/addproductfoot/FoodsFormatThree.vue
@@ -0,0 +1,111 @@
+<template>
+	<div class="bjBox">
+		<div class="threeBox">
+			<p class="addfor">添加规格</p>
+	        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+	          <el-form-item label="规格" prop="name">
+	             <el-input v-model="ruleForm.name"></el-input>
+	          </el-form-item>
+	        <div class="pack">
+			    <p>
+			      <span class="pack1">包装费</span>
+			      <el-input-number v-model="num1" @change="handleChange" :min="0" label="包装费"></el-input-number>
+			    </p>
+			    <p>
+			      <span class="pack2">价格</span>
+			      <el-input-number v-model="num2" @change="handleChange" :min="0" label="价格"></el-input-number>
+			    </p>
+	        </div>
+	        <el-form-item class="clearBtn">
+	            <el-button type="primary" @click="submitForm('ruleForm'),hideopen()">提交</el-button>
+	            <el-button @click="resetForm('ruleForm'),hideopen()">取消</el-button>
+	        </el-form-item>
+	       </el-form>
+	    </div>
+	</div>
+</template>
+<script type="text/ecmascript-6">
+
+	export default{
+		 data() {
+      return {
+        ruleForm: {
+          name: '',
+          num1:0,
+          num2:20
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入规格', trigger: 'blur' },
+          ]
+        }
+      };
+    },
+    methods: {
+    	hideopen(){
+    		this.$emit('openhide',false);
+    	},
+    	handleChange(value) {
+            console.log(value);
+      },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+	}
+</script>
+<style scoped>
+.bjBox{
+	width:100%;
+	height:100%;
+	background: rgba(0,0,0,.4);
+	position:fixed;
+	left:0;
+	top:0;
+	z-index:100;
+}
+.threeBox{
+	width:450px;
+	height:350px;
+	position: absolute;
+	left:50%;
+	top:50%;
+	transform:translate(-50%,-50%);
+	background: #fff;
+	padding-right:50px;
+}
+.addfor{
+	font-size: 18px;
+	font-weight: 600;
+	color:#000;
+	line-height: 50px;
+	padding-left: 20px;
+}
+.clearBtn{
+	clear:both;
+}
+.pack{
+	float:left;
+	padding-left:48px;
+	font-size: 14px;
+    color:#4c576b;
+}
+.pack1{
+	padding-right:8px;
+}
+.pack2{
+	padding-left: 15px;
+	padding-right: 10px;
+}
+	
+</style>
---
+++ src/components/addproduct/addproductfoot/FoodsFormatTwo.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+      <foods-format-three v-show="isOpen" @openhide="hideop"></foods-format-three>
+      <p  @click="open3" >
+        <el-button type="text" class="addspec">添加规格</el-button>
+      </p>
+      
+      <el-table :data="tableData" style="width: 100%;">
+            <el-table-column label="规格" width="180">
+      <template slot-scope="scope">
+        <span>{{ scope.row.spec }}</span>
+      </template>
+      </el-table-column>
+         <el-table-column  label="包装费"  width="180">
+      <template slot-scope="scope">
+         <span>{{ scope.row.pack }}</span>
+      </template>
+      </el-table-column>
+     <el-table-column  label="价格"  width="180">
+      <template slot-scope="scope">
+        <span>{{ scope.row.price }}</span>
+      </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+  </div>
+  
+</template>
+
+<script type="text/ecmascript-6">
+import FoodsFormatThree from "./FoodsFormatThree"
+  export default {
+    data() {
+      return {
+        isOpen:false,
+        tableData: [{
+          spec: '默认',
+          pack: '20',
+          price: '20'
+        }]
+      }
+    },
+    methods: {
+      open3(){
+        this.isOpen=true;
+      },
+      hideop(){
+        this.isOpen=false;
+      },
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      }
+    },
+    components:{
+      FoodsFormatThree
+    }
+  }
+</script>
+<style scoped>
+.addspec{
+  background: #20a1ff;
+  color:#fff;
+  padding:10px 10px;
+  margin-top:30px;
+  clear:both;
+  float:left;
+  margin-left:280px;
+}
+</style>
